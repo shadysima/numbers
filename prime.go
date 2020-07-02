07de0eece26835ee08907277b9b2463af23029ad
shadysima <nschultz4645@gmail.com>
2020-07-02 16:50:08 -0400

    Added IsPrime function

--- prime.go
+++ prime.go
@@ -3,6 +3,25 @@ package numbers
 import "math"
 
 
-func IsPrime(num int) bool {
-	;
+func IsPrime(num uint32) bool {
+	
+	var n int = int(num)
+	if num == 2 {
+		return true
+	}
+	if num % 2 == 0 {
+		return false
+	}
+	
+	var r int = int(math.Floor(math.Sqrt(float64(num))))
+	var step int = 3
+
+	for step < r {
+		if n % step == 0 {
+			return false
+		}
+		step += 2
+	}
+
+	return true
 }
